953713246f41c0af88a1496316861595e817bba9
Abhijit Puri <puriabhijit000@gmail.com>
2021-12-26 22:33:52 +0530

    Firebase authentication

--- app/src/main/java/com/example/quizapp/LogInActivity.kt
+++ app/src/main/java/com/example/quizapp/LogInActivity.kt
@@ -3,16 +3,24 @@ package com.example.quizapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.quizapp.databinding.ActivityLogInBinding
+import android.text.TextUtils
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import soup.neumorphism.NeumorphButton
 
 class LogInActivity : AppCompatActivity() {
+
+
+    private lateinit var etEmail : EditText
+    private lateinit var etPassword: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
 
-        val goToSignUpButton = findViewById<NeumorphButton>(R.id.nbtn_go_to_signup)
+        val goToSignUpButton = findViewById<TextView>(R.id.btn_go_to_signup)
 
         // if user doesn't have an account. Go to sign up page
         goToSignUpButton.setOnClickListener {
@@ -20,7 +28,96 @@ class LogInActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        etEmail = findViewById(R.id.et_email)
+        etPassword = findViewById(R.id.et_password)
+
+        val loginButton = findViewById<NeumorphButton>(R.id.nbtn_login)
+
+        /**
+         * if user clicks on login button
+         */
+        loginButton.setOnClickListener {
+            loginUser()
+        }
+
+    }
+
+    /**
+     * checks if all the entries are filled or not
+     */
+    private fun validateEntries(email: String, password: String): Boolean
+    {
+        if (TextUtils.isEmpty(email)) return false
+
+        if (TextUtils.isEmpty(password)) return false
+
+        return true
+    }
+
+
+    private fun loginUser()
+    {
+        val email = etEmail.text.toString()
+        val password = etPassword.text.toString()
+
+
+        /**
+         * if all the entries are filled login user
+         */
+        if (validateEntries(email, password))
+        {
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).
+                addOnCompleteListener {
+
+                    /**
+                     * if login is successful
+                     */
+                    if (it.isSuccessful) {
+
+                        /**
+                         * currently logged in user
+                         */
+                        val user = it.result.user!!
+
+                        /**
+                         * check if he/she verified email or not
+                         */
+                        if (user.isEmailVerified)
+                        {
+                            /**
+                             * if email is verified then send user to main activity
+                             */
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        /**
+                         * if email is not verified
+                         */
+                        else
+                        {
+                            Toast.makeText(
+                                this, "Please verify your email", Toast.LENGTH_LONG).
+                                show()
+                        }
+
+                    }
+                }
+
+                /**
+                 * if some error occurs while logging in
+                 */
+                .addOnFailureListener {
+
+                    Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+                 }
+
+
+        }
+
 
 
     }
+
 }
--- app/src/main/java/com/example/quizapp/SignUpActivity.kt
+++ app/src/main/java/com/example/quizapp/SignUpActivity.kt
@@ -3,12 +3,21 @@ package com.example.quizapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.quizapp.databinding.ActivitySignUpBinding
+import android.text.TextUtils
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import soup.neumorphism.NeumorphButton
 
 class SignUpActivity : AppCompatActivity()
 
 {
+
+
+    private lateinit var etName : EditText
+    private lateinit var etEmail : EditText
+    private lateinit var etPassword: EditText
 
     override fun onCreate(savedInstanceState: Bundle?)
 
@@ -17,14 +26,120 @@ class SignUpActivity : AppCompatActivity()
 
         setContentView(R.layout.activity_sign_up)
 
-        val goToLoginButton = findViewById<NeumorphButton>(R.id.nbtn_go_to_login)
+        val goToLoginButton = findViewById<TextView>(R.id.btn_go_to_login)
+
+        etName = findViewById(R.id.et_name)
+
+        etEmail = findViewById(R.id.et_email)
+
+        etPassword = findViewById(R.id.et_password)
+
 
         goToLoginButton.setOnClickListener {
             finish()
         }
 
+        val signUpButton = findViewById<NeumorphButton>(R.id.nbtn_login)
 
+        signUpButton.setOnClickListener {
+            signUpUser()
+        }
 
 
     }
+
+
+    private fun validateEntries(name: String, email: String, password: String): Boolean
+    {
+        if (TextUtils.isEmpty(name)) return false
+
+        if (TextUtils.isEmpty(email)) return false
+
+        if (TextUtils.isEmpty(password)) return false
+
+        return true
+    }
+
+
+    private fun signUpUser()
+    {
+
+        val name = etName.text.toString()
+        val email = etEmail.text.toString()
+        val password = etPassword.text.toString()
+
+        /** checks if all the entries are filled or not
+         */
+        if (validateEntries(name, email, password))
+        {
+            FirebaseAuth.getInstance().
+                createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+
+                    /** if user account is created successfully
+                     */
+                    if (it.isSuccessful){
+
+                        val user = FirebaseAuth.getInstance().currentUser!!
+
+                        // add user information to fire-store
+
+                        user.sendEmailVerification()
+                            .addOnCompleteListener {
+
+                                /** if email is sent successfully
+                                 */
+                                if (it.isSuccessful)
+                                {
+                                    Toast.makeText(
+                                        this, "Verification email has been sent to your registered email",
+                                        Toast.LENGTH_LONG).show()
+
+                                    FirebaseAuth.getInstance().signOut()
+
+                                    val intent = Intent(this, LogInActivity::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                }
+                                else {
+
+                                    /**
+                                     * if user entered invalid email
+                                     */
+                                    FirebaseAuth.getInstance().signOut()
+
+                                    Toast.makeText(
+                                        this, "Please enter an valid email id",
+                                        Toast.LENGTH_LONG).show()
+                                }
+
+                            }
+                            /** if some error occurs while sending email
+                             */
+                            .addOnFailureListener {
+                                Toast.makeText(
+                                    this, it.localizedMessage,
+                                    Toast.LENGTH_LONG).show()
+                            }
+                    }
+
+
+
+                }
+                /**
+                 * if some error occurs while creating account
+                 */
+                .addOnFailureListener {
+
+                    Toast.makeText(
+                        this, it.localizedMessage,
+                        Toast.LENGTH_LONG).
+                        show()
+                }
+
+        }
+
+    }
+
+
 }
